9ada1d2e45b0ec98809994836ecbdda7bbc71583
ksh <ksx0001@gmail.com>
2016-04-21 00:03:46 +0800

    flush true

--- danmufm/misc/color_printer.py
+++ danmufm/misc/color_printer.py
@@ -11,19 +11,24 @@ class ColorPrinter:
     @classmethod
     def red(cls, s):
         logger.info(cls.RED + s + cls.END)
+        print(cls.RED + s + cls.END, flush=True)
 
     @classmethod
     def green(cls, s):
         logger.info(cls.GREEN + s + cls.END)
+        print(cls.GREEN + s + cls.END, flush=True)
 
     @classmethod
     def yellow(cls, s):
         logger.info(cls.YELLOW + s + cls.END)
+        print(cls.YELLOW + s + cls.END, flush=True)
 
     @classmethod
     def lightPurple(cls, s):
         logger.info(cls.LIGHT_PURPLE + s + cls.END)
+        print(cls.LIGHT_PURPLE + s + cls.END, flush=True)
 
     @classmethod
     def purple(cls, s):
         logger.info(cls.PURPLE + s + cls.END)
+        print(cls.PURPLE + s + cls.END, flush=True)
